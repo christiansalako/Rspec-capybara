e75030be7acb58c9d13be4fca958ee498a1c00ca
christian Salako <47796176+christiansalako@users.noreply.github.com>
2021-05-15 14:57:40 +0100

    Set up movie factory definition

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :movie do
-    title { "MyString" }
-    director { "MyString" }
-    rate { 1 }
+    title { Faker::movie.title }
+    director { Faker::name.name }
+    rate { Faker::Number.non_zero_digit }
   end
 end
