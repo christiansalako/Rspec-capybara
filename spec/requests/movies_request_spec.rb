a0cbe85cbcc972efc455ecc84341835e3d573259
christian Salako <47796176+christiansalako@users.noreply.github.com>
2021-05-15 15:20:06 +0100

    First request test, testing content of index page!

--- spec/requests/movies_request_spec.rb
+++ spec/requests/movies_request_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Movies", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/movies/index"
-      expect(response).to have_http_status(:success)
+  describe "movies index" do
+    it "shows the right context" do
+      visit '/'
+      expect(page).to have_content('hello world')
     end
   end
-
 end
